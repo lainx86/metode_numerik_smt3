401d00054b823eacbe8aca16759969a186fa7375
feby <febysyarief.dev@gmail.com>
2025-11-09 15:47:00 +0700

    merevisi logika di metode iterasi jacobi

--- modul_7/soal_SPL_1/jacobi_skenario1.m
+++ modul_7/soal_SPL_1/jacobi_skenario1.m
@@ -5,23 +5,33 @@ C = [2 -7 4; 1 9 -6; -3 8 5];
 r = [9; 1; 6];
 e = 10e-4;
 x = zeros(3, 1);
+x_new = zeros(n,1);
+tmax = 0;
 
-for t = 1: 50
-    for i = 1: n
+for t = 1:50
+    for i = 1:n
         temp = r(i);
-        for j = 1: n
-            if i ~=j
+        for j = 1:n
+            if i ~= j
                 temp = temp - C(i,j)*x(j);
             end
         end
-
-        x(t+1,i) = temp/C(i,i);
+        x_new(i) = temp / C(i,i);
     end
 
-    if x(t+1,i) - x(t, i) < e
-        tmax = t+1;
+    if (x_new - x) < e
+        tmax = t + 1;
+        break
     end
+
+    x = x_new; 
 end
 
-disp('Hasil: ');
-disp(tmax);
+if tmax == 0
+    disp('Tidak konvergen dalam 50 iterasi.');
+else
+    disp(['Hasil konvergen pada iterasi ke-', num2str(tmax)]);
+end
+
+disp('Nilai x = ');
+disp(x_new);
--- modul_7/soal_SPL_1/jacobi_skenario4.m
+++ modul_7/soal_SPL_1/jacobi_skenario4.m
@@ -3,25 +3,35 @@ clear all; clc;
 n = 3;
 C = [2 -7 4; 1 9 -6; -3 8 5];
 r = [9; 1; 6];
-e = 10e-5;
-x = [6;3;1];
+e = 1e-5;         
+x = [6; 3; 1];   
+x_new = zeros(n,1);
+tmax = 0;
 
-for t = 1: 50
-    for i = 1: n
+for t = 1:50
+    for i = 1:n
         temp = r(i);
-        for j = 1: n
-            if i ~=j
+        for j = 1:n
+            if i ~= j
                 temp = temp - C(i,j)*x(j);
             end
         end
-
-        x(t+1,i) = temp/C(i,i);
+        x_new(i) = temp / C(i,i);
     end
 
-    if x(t+1,i) - x(t, i) < e
+    if (x_new - x) < e
         tmax = t+1;
+        break
     end
+
+    x = x_new; 
 end
 
-disp('Hasil: ');
-disp(tmax);
+if tmax == 0
+    disp('Tidak konvergen dalam 50 iterasi.');
+else
+    disp(['Hasil konvergen pada iterasi ke-', num2str(tmax)]);
+end
+
+disp('Nilai x = ');
+disp(x_new);
