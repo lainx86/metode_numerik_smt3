c5fa078131793330f8419d4b79e0c41508fd87da
feby <febysyarief.dev@gmail.com>
2025-11-09 15:14:32 +0700

    nambahin beberapa skenario

---
+++ modul_7/soal_SPL_1/gaussSeidel_skenario2.m
@@ -0,0 +1,32 @@
+clear all; clc;
+
+n = 3;
+C = [2 -7 4; 1 9 -6; -3 8 5];
+r = [9; 1; 6];
+e = 10e-5;
+x = [1;2;3];
+
+m = 0;
+
+for i = 1:n
+    temp = r(i);
+    for j = 1:n
+        if i ~= j
+            temp = temp - x(j) * C(i,j);
+        end
+    end
+    temp = temp - x(j)*C(i,j);
+
+    if (temp - x(i)) < e
+        m = m+1;
+    end
+    x(i) = temp;
+end
+
+if m == 0
+    disp('Nilai x1, x2, x3 = ');
+    disp(x);
+else
+    disp('Belum konvergen');
+    disp(x);
+end
