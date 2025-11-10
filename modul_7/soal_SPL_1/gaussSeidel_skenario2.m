7c27e1ad4a570a811373065f37ef821f5e7d303d
feby <febysyarief.dev@gmail.com>
2025-11-10 17:36:56 +0700

    revisi logika loop metode gauss-seidel, modul 7

--- modul_7/soal_SPL_1/gaussSeidel_skenario2.m
+++ modul_7/soal_SPL_1/gaussSeidel_skenario2.m
@@ -5,28 +5,44 @@ C = [2 -7 4; 1 9 -6; -3 8 5];
 r = [9; 1; 6];
 e = 10e-5;
 x = [1;2;3];
-
 m = 0;
+iterasi_maks = 50;
 
-for i = 1:n
-    temp = r(i);
-    for j = 1:n
-        if i ~= j
-            temp = temp - x(j) * C(i,j);
+disp('Iterasi   x1          x2          x3');
+disp('------------------------------------------');
+
+for t = 1:iterasi_maks
+    m = 0; 
+    for i = 1:n
+        temp = r(i);
+        
+        for j = 1:n
+            if i ~= j
+                temp = temp - x(j) * C(i,j);
+            end
         end
+        
+        temp = temp / C(i,i); 
+        if abs(temp - x(i)) < e
+            m = m + 1;
+        end
+        x(i) = temp;
     end
-    temp = temp - x(j)*C(i,j);
+    
+    fprintf('%-8d  %-10.6f  %-10.6f  %-10.6f\n', t, x(1), x(2), x(3));
 
-    if (temp - x(i)) < e
-        m = m+1;
+    if m == n 
+        break;
     end
-    x(i) = temp;
 end
 
-if m == 0
-    disp('Nilai x1, x2, x3 = ');
-    disp(x);
+disp('------------------------------------------');
+
+if m == n
+    disp(['Hasil konvergen pada iterasi ke-', num2str(t)]);
 else
-    disp('Belum konvergen');
-    disp(x);
+    disp(['Tidak konvergen dalam ', num2str(iterasi_maks), ' iterasi.']);
 end
+
+disp('Nilai x = ');
+disp(x);
---
+++ modul_7/test.m
@@ -0,0 +1,1 @@
+% file ini digunakan untuk mengetes skrip saja
