e9defd681442c04fe2fd52bc1edc898d934e8ebc
feby <febysyarief.dev@gmail.com>
2025-11-26 22:47:29 +0700

    Menambahkan metode heun untuk persamaan 1, modul 9

--- modul_9/persamaan1_metode_heun.m
+++ modul_9/persamaan1_metode_heun.m
@@ -32,11 +32,11 @@ fprintf("--------------------------\n")
 fprintf("Iterasi\t x\t\t y (heun)\n")
 fprintf("--------------------------\n")
 for i = 1:length(x)
-    fprintf("%d\t\t %.3f\t %.3f\n", i-1, x(i), y(i));
+    fprintf("%d\t\t %.3f\t %.5f\n", i-1, x(i), y(i));
 end
 fprintf("--------------------------\n")
 
-fprintf("Hasil dari metode Heun adalah: %.3f\n", y(end));
+fprintf("Hasil dari metode Heun adalah: %.5f\n", y(end));
 
 figure;
 plot(x, y, '-o', 'LineWidth', 2, 'MarkerSize', 6);
