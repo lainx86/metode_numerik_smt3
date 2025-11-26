97374fc9a9ea253776e51f28b939d9cb69e2f133
feby <febysyarief.dev@gmail.com>
2025-11-26 14:20:04 +0700

    Menambahkan metode heun pada persamaan 1, modul 9

--- modul_9/persamaan1_metode_heun.m
+++ modul_9/persamaan1_metode_heun.m
@@ -1,47 +1,47 @@
-% File ini hanya digunakan untuk mengetes skrip
-
 clear all; clc;
 
 f = @(x, y) x+y;
 y0 = 1;
 x0 = 0;
 dx = 0.025;
-x_target = 0.1;
+x_target = 0.1; % y(0,1)
 n = (x_target - x0)/dx;
 
-X = zeros(n+1, 1);
-Y = zeros(n+1, 1);
+x = zeros(n+1, 1);
+y = zeros(n+1, 1);
 
-X(1) = x0;
-Y(1) = y0;
+x(1) = x0;
+y(1) = y0;
 
 for i = 1:n
-    xi = X(i);
-    yi = Y(i);
+    y_aksen(i) = f(x(i), y(i));
+    
+    y(i+1) = y(i) + y_aksen(i) * dx;
 
-    Y_absen(i) = f(xi, yi);
-
-    Y(i+1) = yi + f(xi, yi) *dx;
-    Y_absen(i+1) = f(xi+1, yi+1);
-    Y_absen = 1/2* (Y_absen(i) + Y_absen(i+1));
-    Y(i + 1) = Y(i) + Y_absen * dx;
-    X(i+1) = xi + dx;
+    x(i+1) = x(i) + dx;
+    
+    y_aksen(i+1) = f(x(i+1), y(i+1));
+    
+    y_bar_aksen = 0.5 * (y_aksen(i) + y_aksen(i+1));
+    y_prediksi_tampil = y(i+1); 
+    
+    y(i+1) = y(i) + y_bar_aksen * dx;
 end
 
 fprintf("--------------------------\n")
-fprintf("Iterasi\t x\t\t y (euler)\n")
+fprintf("Iterasi\t x\t\t y (heun)\n")
 fprintf("--------------------------\n")
-for i = 1:length(X)
-    fprintf("%d\t\t %.3f\t %.3f\n", i-1, X(i), Y(i));
+for i = 1:length(x)
+    fprintf("%d\t\t %.3f\t %.3f\n", i-1, x(i), y(i));
 end
 fprintf("--------------------------\n")
 
-fprintf("Hasil dari metode Heun adalah: %.3f\n", Y(end));
+fprintf("Hasil dari metode Heun adalah: %.3f\n", y(end));
 
 figure;
-plot(X, Y, '-o', 'LineWidth', 2, 'MarkerSize', 6);
-title(["Grafik Metode Heun dx/dy = x + y"], ["Feby Syarief A-0087-Ose C"]);
-xlabel("delta x");
+plot(x, y, '-o', 'LineWidth', 2, 'MarkerSize', 6);
+title(["Grafik Metode Heun dy/dx = x + y"], ["Feby Syarief A-0087-Ose C"]);
+xlabel("x");
 ylabel("yi + 1");
 grid on;
 
